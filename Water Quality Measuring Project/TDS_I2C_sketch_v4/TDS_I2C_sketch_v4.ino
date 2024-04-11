e37b918a6d8e0132d1d3cb846bacfebfe8310351
psword <101267297+psword@users.noreply.github.com>
2024-04-12 00:18:46 +0300

    Update TDS program file.
    Implements state machine to gain more granular control over the sensor. Fixes error in calculations in adjusting TDS.

--- Water Quality Measuring Project/TDS_I2C_sketch_v4/TDS_I2C_sketch_v4.ino
+++ Water Quality Measuring Project/TDS_I2C_sketch_v4/TDS_I2C_sketch_v4.ino
@@ -1,28 +1,32 @@
 #include <Wire.h>
+#include <algorithm>
 
 // Pin Definitions
 #define SENSOR_INPUT_PIN A0   // Data Wire is plugged into ADC Port
 #define GPIO_PIN 15           // Power Port is GPIO PIN 15
 
 // Timing Intervals
-#define POLLING_INTERVAL 120000   // Interval for polling
-#define SAMPLING_INTERVAL 40000  // Interval for sampling
+#define POLLING_INTERVAL 60000   // Interval for polling
+#define SAMPLING_INTERVAL 100000  // Interval for sampling
 #define SENDING_INTERVAL 37000    // Interval for sending
 
+// Definition for tds sensor reading iterations (sampling points)
+#define tdsSenseIterations  40            // sum of sample point
+
 // Global Variables
-int sensorValue = 0;             // Variable to store sensor value
-float tdsValue = 0;               // Variable to store TDS value
 float adjustedTds = 0;           // Adjusted TDS calculation
-float Voltage = 0;               // Variable to store voltage
-float VC = 3.3;                   // Voltage Constant
+float VC = 3.3;                   // voltage Constant
 int slaveAddress = 8;            // Slave device address
-String FeatherID = "F1: ";       // Device ID
 bool codeExecuted = false;       // Flag for code execution
 const int16_t I2C_SLAVE = 8;     // I2C slave address
 float receivedFloatValue;        // Received float value
 float receivedFloatTemp;         // Received temperature value
 uint8_t messageType = 0x03;      // Message type
 uint8_t messageLength = 8;       // Message length
+int analogBuffer[tdsSenseIterations];     // store the analog value in the array, read from ADC
+int analogBufferTemp[tdsSenseIterations];
+int analogBufferIndex = 0;
+int copyIndex = 0;
 
 // Constants for temperature coefficient calculation
 const float tempCoefficient = 0.02;  // 2% Coefficient calculation
@@ -31,8 +35,23 @@ const float referenceTemp = 25.0;    // Reference temperature for calibration
 // Function Prototypes
 void setup();
 void loop();
-float adjustTds(float rawTDS, float temperature);
+int getMedianNum(int bArray[], int iFilterLen);
+float adjustTds(float sensor, float temperature);
 void transmitSlave();
+void startSensor();
+void stopSensor();
+
+// Configure State Machine
+enum SensorState {
+    SENSOR_OFF,
+    SENSOR_INIT,
+    SENSOR_STABILIZE,
+    SENSOR_READ,
+    SENSOR_SHUTDOWN
+};
+
+SensorState currentState = SENSOR_OFF;
+unsigned long stateStartTime = 0;
 
 void setup() {
     // Initialize Serial Communication
@@ -47,84 +66,143 @@ void setup() {
 
     // Execute code only once
     if (!codeExecuted) {
-        int tdsSenseIterations = 40;
-        digitalWrite(GPIO_PIN, HIGH); // Power on the sensor
-        delay(1000); // Wait for sensor stabilization
-
-        // Perform multiple readings for calibration
-        for (int i = 0; i < tdsSenseIterations; i++) {
-            delay(500); // Wait for sensor stabilization
-            sensorValue = analogRead(SENSOR_INPUT_PIN); // Read analog value from sensor
-            Voltage = sensorValue * VC / 1024.0; // Convert analog reading to voltage
-            tdsValue = (133.42 / Voltage * Voltage * Voltage - 255.86 * Voltage * Voltage + 857.39 * Voltage) * 0.5; // Convert voltage value to TDS value
-        }
-
-        digitalWrite(GPIO_PIN, LOW); // Power off the sensor
-        Serial.print("Setup Complete.");
+        delay(5000);
+        Serial.println("Setup Complete.");
         codeExecuted = true;
     }
 }
 
 void loop() {
-    static unsigned long pollingTime = millis();
 
-    // Polling Interval Check
-    if (millis() - pollingTime > POLLING_INTERVAL) {
-        Wire.requestFrom(I2C_SLAVE, 8); // Request 8 bytes from slave device #8
-
-        while (Wire.available()) { // Slave may send less than requested
-            uint8_t receivedMessageType = Wire.read(); // Read the message type byte
-            uint8_t receivedMessageLength = Wire.read(); // Read the message length byte
-
-            // Handle message type 0x02 (temperature value)
-            if (receivedMessageType == 0x02 && receivedMessageLength == 8) {
-                uint8_t byteArray[sizeof(float)]; // Create an array to store the bytes of the float
-
-                for (int i = 0; i < receivedMessageLength; i++) {
-                    byteArray[i] = Wire.read(); // Read bytes of the message body into byteArray
-                }
-
-                // Reconstruct the float value from byteArray
-                float *ptr = reinterpret_cast<float*>(byteArray); // Type punning
-                receivedFloatValue = *ptr; // Assign the reconstructed float value
-                receivedFloatTemp = receivedFloatValue;
+    switch (currentState) {
+        case SENSOR_OFF:
+            // Do nothing until instructed to turn on the sensor
+            break;
+        case SENSOR_INIT:
+            // Initialize sensor
+            digitalWrite(GPIO_PIN, HIGH); // Power on the sensor
+            stateStartTime = millis();
+            currentState = SENSOR_STABILIZE;
+            break;
+        case SENSOR_STABILIZE:
+            // Wait for sensor stabilization
+            if (millis() - stateStartTime >= 5000) { // Wait for 5 seconds
+                currentState = SENSOR_READ; // Move to next state
             }
+            break;
+        case SENSOR_READ:
+            // Read sensor data
+            adjustedTds = readAndAdjustTds(receivedFloatTemp); // Replace 'temperature' with the actual temperature value to adjust the TDS
+            delay(250);
+            if (millis() - stateStartTime >= 15000) { // Wait for 10 seconds
+              currentState = SENSOR_SHUTDOWN; // Move to next state
+            }
+            // Serial.println(adjustedTds); // Uncomment for debugging
+            break; 
+        case SENSOR_SHUTDOWN:
+            // Power off the sensor
+            digitalWrite(GPIO_PIN, LOW);
+            currentState = SENSOR_OFF; // Reset to initial state
+            break;
+    }
+    
+    // Polling Interval Check
+    static unsigned long pollingTime = millis();
+    if (millis() - pollingTime > POLLING_INTERVAL) {
+        actuateData();
+        pollingTime = millis();
         }
 
-        pollingTime = millis();
-    }
-
-    static unsigned long samplingTime = millis();
-
     // Sampling Interval Check
+    static unsigned long samplingTime = millis();
     if (millis() - samplingTime > SAMPLING_INTERVAL) {
-        digitalWrite(GPIO_PIN, HIGH); // Power on the sensor
-        delay(1500); // Wait for sensor stabilization
-        sensorValue = analogRead(SENSOR_INPUT_PIN); // Read analog value from sensor
-        Voltage = sensorValue * VC / 1024.0; // Convert analog reading to voltage
-        tdsValue = (133.42 / Voltage * Voltage * Voltage - 255.86 * Voltage * Voltage + 857.39 * Voltage) * 0.5; // Convert voltage value to TDS value
-        adjustedTds = adjustTds(tdsValue, receivedFloatTemp); // Calculate the adjusted TDS
-        delay(5000); // Wait for sensor stabilization
-        digitalWrite(GPIO_PIN, LOW); // Power off the sensor
+        startSensor();
         samplingTime = millis();
     }
 
-    static unsigned long sendingTime = millis();
-
     // Sending Interval Check
+    static unsigned long sendingTime = millis();
     if (millis() - sendingTime > SENDING_INTERVAL) {
-        transmitSlave(); // Transmit temperature to slave device
+        transmitSlave(); // Transmit TDS to slave device
         sendingTime = millis();
     }
 }
 
-// Function to calculate adjusted TDS reading based on temperature
-float adjustTds(float rawTDS, float temperature) {
+void startSensor() {
+    currentState = SENSOR_INIT;
+}
+
+void stopSensor() {
+    currentState = SENSOR_OFF;
+}
+
+void actuateData() {
+    if (Wire.requestFrom(I2C_SLAVE, 8) != 8) {
+        // Error: Did not receive expected number of bytes
+        // Serial.println("Error retrieving temperature.");
+        return;
+    }
+
+    // Request 8 bytes from slave device #8
+    uint8_t receivedMessageType = Wire.read(); // Read the message type byte
+    uint8_t receivedMessageLength = Wire.read(); // Read the message length byte
+
+    // Handle message type 0x02 (temperature value)
+    if (receivedMessageType == 0x02 && receivedMessageLength == 8) {
+        receivedFloatTemp = readFloatFromWire(receivedMessageLength);
+    }
+}
+
+float readFloatFromWire(uint8_t length) {
+    uint8_t byteArray[sizeof(float)]; // Create an array to store the bytes of the float
+    for (int i = 0; i < length; i++) {
+        byteArray[i] = Wire.read(); // Read bytes of the message body into byteArray
+    }
+    // Reconstruct the float value from byteArray
+    float *ptr = reinterpret_cast<float*>(byteArray); // Type punning
+    return *ptr; // Return reconstructed float value
+}
+
+
+// Function to read analog value from sensor and store in buffer
+void analogReadAction() {
+    int sensorValue = analogRead(SENSOR_INPUT_PIN);
+    // Serial.println(sensorValue); // Uncomment for debugging
+    analogBuffer[analogBufferIndex] = sensorValue;
+    analogBufferIndex = (analogBufferIndex + 1) % tdsSenseIterations; // Circular buffer       
+}
+
+// Function to compute median value from buffer
+int computeMedian() {
+    int sortedBuffer[tdsSenseIterations];
+    memcpy(sortedBuffer, analogBuffer, tdsSenseIterations * sizeof(int));
+    std::sort(sortedBuffer, sortedBuffer + tdsSenseIterations);
+    
+    if (tdsSenseIterations % 2 == 0) {
+        return (sortedBuffer[tdsSenseIterations / 2 - 1] + sortedBuffer[tdsSenseIterations / 2]) / 2;
+    } else {
+        return sortedBuffer[tdsSenseIterations / 2];
+    }
+}
+
+// Function to adjust TDS reading based on temperature
+float adjustTds(float voltage, float temperature) {
     // Calculate temperature correction factor
     float tempCorrection = 1.0 + tempCoefficient * (temperature - referenceTemp);
+    
+    // Calculate compensation voltage
+    float compensationVoltage = voltage / tempCorrection;
+
     // Adjust TDS reading based on temperature
-    float adjustedTds = rawTDS + tempCorrection;
-    return adjustedTds;
+    return (133.42 / compensationVoltage * compensationVoltage * compensationVoltage - 255.86 * compensationVoltage * compensationVoltage + 857.39 * compensationVoltage) * 0.5;
+}
+
+// Combined function for reading analog value and adjusting TDS
+float readAndAdjustTds(float temperature) {
+    analogReadAction();
+    int medianSensorValue = computeMedian();
+    float averageVoltage = medianSensorValue * VC / 1024.0;
+    return adjustTds(averageVoltage, temperature);
 }
 
 // Transmit to slave
@@ -132,11 +210,11 @@ void transmitSlave() {
     Wire.beginTransmission(slaveAddress); // Start I2C transmission with slave (Arduino)
     Wire.write(messageType); // Send message type
     Wire.write(messageLength); // Send message length
-    uint8_t byteArray[sizeof(float)]; // Create byte array to store temperature
-    memcpy(byteArray, &adjustedTds, sizeof(float)); // Copy temperature value to byte array
+    uint8_t byteArray[sizeof(float)]; // Create byte array to store TDS
+    memcpy(byteArray, &adjustedTds, sizeof(float)); // Copy TDS value to byte array
 
     for (int i = 0; i < sizeof(float); i++) {
-        Wire.write(byteArray[i]); // Send temperature byte by byte
+        Wire.write(byteArray[i]); // Send TDS byte by byte
     }
 
     Wire.endTransmission(); // End transmission
